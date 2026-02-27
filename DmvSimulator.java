87f11eeecc483ce4bafeddccb04e143f3b8548e5
nharding925 <nharding925@gmail.com>
2026-02-26 19:20:13 -0500

    Added line loop and random number

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -1,4 +1,26 @@
+import java.util.Random;
+
 public class DmvSimulator {
     public static void main(String[] args) {
+        Random random = new Random();
+        
+        System.out.println("Welcome to the DMV!");
+        
+        //print random number 1-100
+        int userNumber = random.nextInt(100) + 1;
+        System.out.println("Your number is: " + userNumber);
+        
+        int current = userNumber + 1;
+        if (current > 100) current = 1;
+        
+        //print all the numbers that arent users
+        while (current != userNumber) {
+            System.out.println("Now serving: " + current);
+            current++;
+            if (current > 100) current = 1;
+        }
+        
+        System.out.println("Now serving: " + userNumber);
+        System.out.println("Sorry, you don't have the required paperwork.");
     }
 }
